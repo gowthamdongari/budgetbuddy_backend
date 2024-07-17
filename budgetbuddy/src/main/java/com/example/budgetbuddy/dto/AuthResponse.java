bbccce4fe9e8a4ace4f04bc39236dafdfc878046
Ramaruva <ramaruva54@gmail.com>
2024-07-16 20:51:13 -0500

    added intial loginregister controller

---
+++ budgetbuddy/src/main/java/com/example/budgetbuddy/dto/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.example.budgetbuddy.dto;
+
+public record AuthResponse(String token) {
+}
--- budgetbuddy/src/main/java/com/example/budgetbuddy/services/LoginService.java
+++ budgetbuddy/src/main/java/com/example/budgetbuddy/services/LoginService.java
@@ -1,11 +1,20 @@
 package com.example.budgetbuddy.services;
 
 import com.example.budgetbuddy.dao.UsersRepository;
+import com.example.budgetbuddy.dto.AuthResponse;
+import com.example.budgetbuddy.dto.LoginRequest;
+import com.example.budgetbuddy.dto.RegisterRequest;
+import com.example.budgetbuddy.modal.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LoginService implements UserDetailsService {
@@ -13,10 +22,38 @@ public class LoginService implements UserDetailsService {
     private final UsersRepository usersRepository;
     private  final  JwtService jwtService;
 
-    public LoginService(UsersRepository usersRepository, JwtService jwtService) {
+    private final PasswordEncoder passwordEncoder;
+
+    public LoginService(UsersRepository usersRepository, JwtService jwtService, @Lazy PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
     }
+
+    public  boolean registration(RegisterRequest registerRequest){
+          if(usersRepository.existsByEmail(registerRequest.email())){
+              return  false;
+          }
+        Users users = new Users();
+        users.setEmail(registerRequest.email());
+        users.setName(registerRequest.name());
+        users.setPasswordHash(passwordEncoder.encode(registerRequest.password()));
+        usersRepository.save(users);
+        return  true; 
+    }
+
+    public AuthResponse login(LoginRequest loginRequest){
+        Optional<Users> usersOptional = usersRepository.findByEmail(loginRequest.email());
+        if(usersOptional.isPresent()){
+            Users users = usersOptional.get();
+            if(passwordEncoder.matches(loginRequest.password(),users.getPassword())){
+                 String token = jwtService.generateToken(users);
+                 return  new AuthResponse(token);
+            }
+        }
+        return  null;
+    }
+
 
     public String passwordHashing(String password){
         String salt = BCrypt.gensalt(12);
@@ -25,6 +62,8 @@ public class LoginService implements UserDetailsService {
     public boolean checkPassword(String password, String hashedPassword){
           return  BCrypt.checkpw(password,hashedPassword);
     }
+
+
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
